3b55a2acfc38b6b898eeb1a0bd9805d4ea9184f8
David P. Federl <david-peter.federl@federl.digital>
2024-05-16 22:09:05 +0200

    wip(extension-board): add test for wifi and adxl345b

--- src/ExtensionBoard.c
+++ src/ExtensionBoard.c
@@ -1,102 +1,98 @@
 #define SOURCE_FILE "MAIN"
 
+#include "Adxl345b.h"
+#include "Adxl345bTypedefs.h"
 #include "Common.h"
 #include "Esp.h"
-#include "FreeRtosQueueWrapper.h"
-#include "FreeRtosTaskWrapper.h"
 #include "Network.h"
 
-#include <hardware/watchdog.h>
-#include <pico/bootrom.h>
+#include <hardware/i2c.h>
 #include <pico/stdlib.h>
 
-/* region LED HANDLER */
-
-#define LED0_PIN 22
-#define LED1_PIN 24
-#define LED2_PIN 25
-
-void led_init(uint8_t gpio) {
-    gpio_init(gpio);
-    gpio_set_dir(gpio, GPIO_OUT);
-}
-
-void leds_init() {
-    led_init(LED0_PIN);
-    led_init(LED1_PIN);
-    led_init(LED2_PIN);
-}
-
-void leds_all_on() {
-    gpio_put(LED0_PIN, 1);
-    gpio_put(LED1_PIN, 1);
-    gpio_put(LED2_PIN, 1);
-}
-
-void leds_all_off() {
-    gpio_put(LED0_PIN, 0);
-    gpio_put(LED1_PIN, 0);
-    gpio_put(LED2_PIN, 0);
-}
-
-/* endregion LED HANDLER */
-
-_Noreturn void ledTask(void) {
-    while (true) {
-        PRINT("LEDs should now turn on");
-        leds_all_on();
-        freeRtosTaskWrapperTaskSleep(2000);
-        PRINT("LEDs should now turned off");
-        leds_all_off();
-        freeRtosTaskWrapperTaskSleep(2000);
-    }
-}
-
-/*! \brief Goes into bootloader mode when 'r' is pressed
- */
-_Noreturn void enterBootModeTask(void) {
-    while (true) {
-        if (getchar_timeout_us(10) == 'r' || !stdio_usb_connected()) {
-            PRINT_DEBUG("Boot Mode request detected.");
-            reset_usb_boot(0, 0);
-        }
-
-        // Watchdog update needs to be performed frequent, otherwise the device will crash
-        watchdog_update();
-        PRINT_DEBUG("watchdog updated");
-        freeRtosTaskWrapperTaskSleep(1000);
-    }
-}
-
-void init(void) {
-    // First check if we crash last time -> reboot into boot rom mode
-    if (watchdog_enable_caused_reboot()) {
-        reset_usb_boot(0, 0);
-    }
-
-    // init usb
+static void initializeIo(void) {
     stdio_init_all();
-    // waits for usb connection, REMOVE to continue without waiting for connection
     while ((!stdio_usb_connected())) {}
+}
 
-    // Checks connection to ESP and initialize ESP
+static void testWifiModule(void) {
+    PRINT("=== TEST WIFI START ===");
+
     espInit();
+    PRINT("  ESP initialized!");
 
-    // enables watchdog to check for reboots
-    watchdog_enable(2000, 1);
+    networkErrorCode_t error;
+
+    error = networkTryToConnectToNetworkUntilSuccessful();
+    if (error != NETWORK_NO_ERROR) {
+        PRINT("  Failed to Connect to WIFI (0x%02X)", error);
+        return;
+    }
+    PRINT("  \033[0;32mPASSED\033[0m");
+    PRINT("=== TEST WIFI DONE ===");
+}
+static void getSerialNumber() {
+    uint8_t serialNumber = 0;
+
+    PRINT("Requesting serial number:");
+    adxl345bErrorCode_t errorCode = adxl345bReadSerialNumber(&serialNumber);
+    if (errorCode == ADXL345B_NO_ERROR) {
+        PRINT("  Expected: 0xE5, Actual: 0x%02X", serialNumber);
+        PRINT(serialNumber == 0xE5 ? "  \033[0;32mPASSED\033[0m" : "  \033[0;31mFAILED\033[0m;");
+    } else {
+        PRINT("  \033[0;31mFAILED\033[0m adxl345b_ERROR: %02X", errorCode);
+    }
+}
+static void makeSelfTest() {
+    PRINT("Start self test:");
+    int delta_x, delta_y, delta_z;
+    adxl345bErrorCode_t errorCode = adxl345bPerformSelfTest(&delta_x, &delta_y, &delta_z);
+    PRINT("  X: %iLSB, Y: %iLSB, Z: %iLSB", delta_x, delta_y, delta_z);
+    if (errorCode == ADXL345B_NO_ERROR) {
+        PRINT("  \033[0;32mPASSED\033[0m");
+    } else {
+        PRINT("  \033[0;31mFAILED\033[0m; adxl345b_ERROR: %02X", errorCode);
+    }
+}
+static void testAccelerometer(void) {
+    PRINT("=== TEST ADXL345b START");
+    adxl345bErrorCode_t error;
+
+    error = adxl345bInit(i2c1, ADXL345B_I2C_ALTERNATE_ADDRESS);
+    if (error != ADXL345B_NO_ERROR) {
+        PRINT("  Initialize Sensor failed! (0x%02X)", error);
+        return;
+    }
+    PRINT("  Sensor initialized successful!");
+
+    getSerialNumber();
+    makeSelfTest();
+
+    PRINT("=== TEST ADXL345b DONE");
+}
+static void testAmplifier(void) {
+    // TODO
+}
+_Noreturn static void testBoard(void) {
+    while (1) {
+        char input = getchar_timeout_us(UINT32_MAX);
+
+        switch (input) {
+        case 'w':
+            testWifiModule();
+            break;
+        case 'a':
+            testAccelerometer();
+            break;
+        case 'm':
+            testAmplifier();
+            break;
+        default:
+            PRINT("WRONG INPUT");
+        }
+    }
 }
 
 int main() {
-    // initialize hardware
-    init();
-    leds_init();
-
-    networkTryToConnectToNetworkUntilSuccessful();
-
-    // add freeRTOS tasks
-    freeRtosTaskWrapperRegisterTask(enterBootModeTask, "enterBootModeTask", 0, FREERTOS_CORE_0);
-    freeRtosTaskWrapperRegisterTask(ledTask, "ledTask", 0, FREERTOS_CORE_1);
-
-    // starts freeRTOS tasks
-    freeRtosTaskWrapperStartScheduler();
+    initializeIo();
+    testBoard();
 }
